33e176c0f82fb8c8534d39a658a13e39d7929e99
Jonathan Pike <code@staite.net>
2014-01-20 17:53:30 +0000

    Update 404.php

--- core/404.php
+++ core/404.php
@@ -1,31 +1,22 @@
-<?php
+<?php 
 
 define('ABLE_NO_AUTH', true);
-
 require_once('init.php');
 
 if (Context::$conf['content_dir'] !== null)
 {
-   // convert any extra slashes to _ for filenames
    $path = str_replace('/', '_', Request::$url->local);
-   $file_pattern = sprintf('%s/%s.*',
-      Context::$conf['content_dir'],
-      $path);
-   
-   if (count($files = glob($file_pattern)) > 0)
-   {
-      require($files[0]);
-      return;
-   }
+   $file_pattern = sprintf('%s/%s.html', 
+      Context::$conf['content_dir'], $path);   
+   if ($files = glob($file_pattern))
+      return require($files[0]);
 }
 
 header('HTTP/1.0 404 Not Found');
-
 if (Context::$conf['error_doc_404'] !== null)
    die(require(Context::$conf['error_doc_404']));
 
 ?>
-
 <!doctype html>
 <html>
    <head>
