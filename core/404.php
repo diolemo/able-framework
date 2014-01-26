c848aaf532345aa02d4b85bb40417d6659814238
Jonathan Pike <code@staite.net>
2014-01-26 22:01:04 +0000

    Update 404.php

--- core/404.php
+++ core/404.php
@@ -12,10 +12,10 @@ if (Context::$conf['content_dir'] !== null)
       return require($files[0]);
 }
 
-Content::$__auto_render = false;
 header('HTTP/1.0 404 Not Found');
 if (Context::$conf['error_doc_404'] !== null)
    die(require(Context::$conf['error_doc_404']));
+Content::$__auto_render = false;
 
 ?>
 <!doctype html>
