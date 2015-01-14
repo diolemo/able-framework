83a8b10ceb860b7fbb51e3eac9590344a3ae3ed1
Jonathan Pike <code@staite.net>
2015-01-14 04:30:22 +0100

    ..

--- core/context.php
+++ core/context.php
@@ -2,6 +2,10 @@
 
 class Context
 {
+   const ENV_DEVELOPMENT = E_ALL;
+   const ENV_TESTING     = E_ERROR | E_WARNING | E_PARSE;
+   const ENV_PRODUCTION  = 0;
+
    public static $cache;
    public static $conf;
    public static $db;
---
+++ core/env.php
@@ -0,0 +1,7 @@
+<?php
+
+// set error reporting to desired level
+error_reporting(Context::$conf['environment']);
+ini_set('display_errors', 1);
+
+?>
